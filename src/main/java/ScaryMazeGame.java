a09a12ef966cd1b8e297051c329bccc3c07401fb
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-10-27 13:55:15 -0700

    Draw the intro screen.

--- src/main/java/ScaryMazeGame.java
+++ src/main/java/ScaryMazeGame.java
@@ -1,8 +1,18 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class ScaryMazeGame extends JComponent {
 
-    public static void main(String args[]) {
+    BufferedImage intro;
+
+    public ScaryMazeGame() throws IOException {
+        intro = ImageIO.read(getClass().getResource("Intro.png"));
+    }
+
+    public static void main(String args[]) throws IOException {
         JFrame window = new JFrame("Scary Maze by Aaron");
         ScaryMazeGame game = new ScaryMazeGame();
         window.add(game);
@@ -10,5 +20,18 @@ public class ScaryMazeGame extends JComponent {
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 600);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(0, 0, 800, 600);
+
+        g.drawImage(intro, 0, 0, null);
     }
 }
